9ef423f232ca03232fadff6325d3f97a08b68994
Romain-OC <54536652+Romain-OC@users.noreply.github.com>
2019-11-04 13:22:21 +0100

    Add files via upload

--- Main.py
+++ Main.py
@@ -12,8 +12,8 @@ class Main :
     cont = input("Entrer une autre addresse ? o pour oui, n pour non \n")
     while ( not(cont == 'n') and not(cont == 'o')):
         cont = input("veuiller entrer une valeur correcte. o pour oui, n pour non \n")
-        while not (cont == 'n'):
-            address = input("Entrez l'addresse suivante \n")
+    while cont == 'o':
+        address = input("Entrez l'addresse suivante \n")
         interfaces.address.append(address)
         cont = input("Entrer une autre addresse ? o pour oui, n pour non \n")
         while (not (cont == 'n') and not (cont == 'o')) :
@@ -44,26 +44,23 @@ class Main :
         parefeu.interface = interfaces.address
         parefeu.initFichier(parefeu.interface)
         parefeu.standardConfig()
-        #cont = input("Voulez vous ouvrir certain port ? o pour oui, n pour non \n")
-        #while (not (cont == 'n') and (cont == 'o')) : 
-            #cont = input("veuiller entrer une valeur correcte. o pour oui, n pour non \n")
-        #while cont == 'o' :
-            #temp = input("quel est le port à ouvrir ? \n")
-            #parefeu.openPort.append(temp)
-            #temp= input("quel est le protocol du port entré ? \n")
-            #parefeu.protocol.append(temp)
-            #cont = input("Ouvrir un autre Port ? o pour oui, n pour non \n")
-            #while (not (cont == 'n') and not (cont == 'o')) : 
-                #cont = input("veuiller entrer une valeur correcte. o pour oui, n pour non \n")
-            #while not (cont =='n'):
-                #temp= input("quel est le port à ouvrir ? \n")
-                #parefeu.openPort.append(temp)
-                #temp= input("quel est le protocol du port entré ? \n")
-                #parefeu.protocol.append(temp)
-                #cont = input("Ouvrir un autre Port ? o pour oui, n pour non \n")
-        parefeu.protocol.extend(('dns','icmp'))
-        parefeu.openPort.extend(('53','1'))
+        cont = input("Voulez vous ouvrir certain port ? o pour oui, n pour non \n")
+        while (not (cont == 'n') and (cont == 'o')) : 
+            cont = input("veuiller entrer une valeur correcte. o pour oui, n pour non \n")
+        while cont == 'o' :
+            temp = input("quel est le port à ouvrir ? \n")
+            parefeu.openPort.append(temp)
+            temp= input("quel est le protocol du port entré ? \n")
+            parefeu.protocol.append(temp)
+            cont = input("Ouvrir un autre Port ? o pour oui, n pour non \n")
+            while (not (cont == 'n') and not (cont == 'o')) : 
+                cont = input("veuiller entrer une valeur correcte. o pour oui, n pour non \n")
+            while not (cont =='n'):
+                temp= input("quel est le port à ouvrir ? \n")
+                parefeu.openPort.append(temp)
+                temp= input("quel est le protocol du port entré ? \n")
+                parefeu.protocol.append(temp)
+                cont = input("Ouvrir un autre Port ? o pour oui, n pour non \n")
     parefeu.OpenPort(parefeu.openPort,parefeu.protocol)
     parefeu.endConfig()
-    cont = input("Le système va redémarrer veuillez appuyer sur entrée pour continuer \n")
     subprocess.run('reboot',shell=True)
