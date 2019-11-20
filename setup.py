49aadf4a4dd3d44388b8c31cce600aedc46d2adb
Romain-OC <54536652+Romain-OC@users.noreply.github.com>
2019-11-20 16:59:44 +0100

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from cx_Freeze import setup, Executable
-
-# On appelle la fonction setup
-setup(
-    name = "main",
-    version = "0.1",
-    description = "Configuration network",
-    executables = [Executable("main.py")],
-)
